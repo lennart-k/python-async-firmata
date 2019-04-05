cd8d2b00f8355d394af4c4b5271a491ebd1a68f6
lennart-k <lekahade@gmail.com>
2019-04-05 16:25:16 +0200

    Fixed the installation

--- setup.py
+++ setup.py
@@ -17,5 +17,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=open("requirements.txt").read().split("\n")
+    install_requires=[
+        "pyserial-asyncio"
+    ]
 )
