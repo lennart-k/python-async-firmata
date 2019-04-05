c6a1f4d4284a587d44d9ae80c6a7e56363f2f745
lennart-k <lekahade@gmail.com>
2019-04-05 16:28:48 +0200

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="async-firmata",
-    version="0.0.2",
+    version="0.0.3",
     author="Lennart K",
     description="An asynchronous interface for the Firmata protocol",
     long_description=long_description,
